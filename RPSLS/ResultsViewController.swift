267d502b16408c7e10f16f56f6e62c458bc76851
Donny Davis <donnywdavis@icloud.com>
2019-03-18 20:52:18 +0100

    Create ResultsViewController UI

---
+++ RPSLS/ResultsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultsViewController.swift
+//  RPSLS
+//
+//  Created by Donald Davis on 18.03.19.
+//  Copyright © 2019 Donny Davis. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
