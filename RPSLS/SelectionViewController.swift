80729b7d3255d99a7783cd544c12401946bfbf31
Donny Davis <donnywdavis@icloud.com>
2019-03-18 20:58:03 +0100

    Connect images to tap gesture

--- RPSLS/SelectionViewController.swift
+++ RPSLS/SelectionViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class SelectionViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var rock: UIImageView!
+    @IBOutlet weak var paper: UIImageView!
+    @IBOutlet weak var scissors: UIImageView!
+    @IBOutlet weak var lizard: UIImageView!
+    @IBOutlet weak var spock: UIImageView!
+
+    private var tapGesture: UITapGestureRecognizer {
+        return UITapGestureRecognizer(target: self, action: #selector(imageTapped(_:)))
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        rock.addGestureRecognizer(tapGesture)
+        paper.addGestureRecognizer(tapGesture)
+        scissors.addGestureRecognizer(tapGesture)
+        lizard.addGestureRecognizer(tapGesture)
+        spock.addGestureRecognizer(tapGesture)
+    }
+
+    @objc private func imageTapped(_ sender: UITapGestureRecognizer) {
+    }
 }
 
